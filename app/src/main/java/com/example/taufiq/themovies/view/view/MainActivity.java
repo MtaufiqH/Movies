ed4972e48a9b29747f7ffb7e197915325d20ddc6
mtaufiqh62@gmail.com <123darussalam>
2019-09-26 09:06:14 +0800

    state save (done)

--- app/src/main/java/com/example/taufiq/themovies/view/view/MainActivity.java
+++ app/src/main/java/com/example/taufiq/themovies/view/view/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 
 import com.example.taufiq.themovies.R;
 import com.example.taufiq.themovies.view.view.fragment.MoviesFragment;
@@ -16,8 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     public static final String KEY_FRAGMENT = "fragment";
-    private Fragment content = new MoviesFragment();
-    private Fragment content1 = new TvFragment();
+
+    private SparseArray<Fragment.SavedState> savedStateSparseArray = new SparseArray<>();
+
+    private int current = R.id.movies_menu;
+
+    private static final String SAVED_STATE_CONTAINER_KEY = "ContainerKey";
+    private static final String SAVED_STATE_CURRENT_TAB_KEY = "CurrentTabKey";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +38,21 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(false);
         }
 
-        if (savedInstanceState == null) {
-            initFragment(content);
-        } else {
-            content = getSupportFragmentManager().getFragment(savedInstanceState, KEY_FRAGMENT);
-            initFragment(content);
-        }
+
+        if (savedInstanceState != null) {
+            savedStateSparseArray = savedInstanceState.getSparseParcelableArray(SAVED_STATE_CONTAINER_KEY);
+            current = savedInstanceState.getInt(SAVED_STATE_CURRENT_TAB_KEY);
+        } else
+            swapFragments(current, KEY_FRAGMENT, new MoviesFragment());
+
 
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        getSupportFragmentManager().putFragment(outState, KEY_FRAGMENT, content);
         super.onSaveInstanceState(outState);
+        outState.putSparseParcelableArray(SAVED_STATE_CONTAINER_KEY, savedStateSparseArray);
+        outState.putInt(SAVED_STATE_CURRENT_TAB_KEY, current);
     }
 
     private void bottomNavigationSetup(BottomNavigationView bottomNavigationView) {
@@ -54,11 +63,11 @@ public class MainActivity extends AppCompatActivity {
                     int nav_id = menuItem.getItemId();
                     switch (nav_id) {
                         case R.id.movies_menu:
-                            initFragment(new MoviesFragment());
+                            swapFragments(nav_id, "Movie", new MoviesFragment());
                             break;
 
                         case R.id.tv_menu:
-                            initFragment(new TvFragment());
+                            swapFragments(nav_id, "Tv Show", new TvFragment());
                             break;
 
                         case R.id.settings_id:
@@ -67,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
                             break;
                     }
 
-                    initFragment(content);
                     return true;
                 });
     }
@@ -80,14 +88,27 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.fragment_container, fragment)
                 .commit();
 
+
     }
 
 
+    private void saveFragmentState(int actionId) {
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (currentFragment != null) {
+            savedStateSparseArray.put(current, getSupportFragmentManager().saveFragmentInstanceState(currentFragment));
+        }
 
-    /*@Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }*/
+        current = actionId;
+
+    }
+
+
+    private void swapFragments(int actionId, String key, Fragment fragment) {
+        if (getSupportFragmentManager().findFragmentByTag(key) == null) {
+            saveFragmentState(actionId);
+            initFragment(fragment);
+        }
+    }
 
 
 }
